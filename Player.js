398b2ab6c6de905b3ccaf5af82fac090356e4261
Minghai Chen <chenminghai79@gmail.com>
2023-10-20 19:03:04 +0100

    instalacion basica

---
+++ Player.js
@@ -0,0 +1,15 @@
+class Player{
+    name;
+
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+// Esto son pruebas (ELIMINAR A PARTIR DE AQUI)
+
+let player1 = new Player("Juan");
+console.log(`Nombre del jugador: ${player1}`); // Juan
+
+
+

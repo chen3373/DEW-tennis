d9b44fe264b3ce2194b538b7dabcf55e8c93edad
Minghai Chen <chenminghai79@gmail.com>
2023-10-23 17:17:00 +0100

    test match done

--- Match.js
+++ Match.js
@@ -1,5 +1,12 @@
 class Match{
-
-
+    players = [];
+    winner = null;
+    insertPlayer(player){
+        if(this.players.length<2) return false;{
+            this.players.push(player);
+            return true;
+        }
+    }
+    
 }
-module.exports
+module.exports = Match;
--- Match.test.js
+++ Match.test.js
@@ -1,7 +1,42 @@
 const Match = require('./Match');
+const Player = require('./Player');
 
 test('Crear un partido, ', () => {
     const partido1 = new Match();
-    expect(partido1).toEqual({});
-    expect(partido1 instanceof Match).toBeTrue();
+    expect(partido1).toEqual({
+        players: [],
+        winner: null
+    });
+    expect(partido1 instanceof Match).toBeTruthy();
+});
+
+test('Insertar un jugador en un partido sin jugadores', ()=>{
+    const partido1 = new Match();
+    const jugador1 = new Player('Pepe');
+    partido1.insertPlayer(jugador1);
+    const jugador2 = new Player('Juan');
+    partido1.insertPlayer(jugador2);
+
+    //expect(partido1.players[0]).toBe(jugador1);
+    //expect(partido1.players[1]).toBe(jugador2);
+    expect(partido1.players.length).toBe(2);
+    expect(partido1.players).toContain(jugador1);
+    expect(partido1.players).toContain(jugador2);
+;
+});
+
+test('Insertar 3 jugadores, solo deben estar los 2 primeros, el último no se inserta', ()=>{
+    const partido1 = new Match();
+    const jugador1 = new Player('Pepe');
+    partido1.insertPlayer(jugador1);
+    const jugador2 = new Player('Juan');
+    partido1.insertPlayer(jugador2);
+    const jugador3 = new Player('Ana');
+    partido1.insertPlayer(jugador3);
+
+    expect(partido1.players.length).toBe(2);
+    expect(partido1.players).toContain(jugador1);
+    expect(partido1.players).toContain(jugador2);
+    expect(partido1.players).not.toContain(jugador3);
+
 });
